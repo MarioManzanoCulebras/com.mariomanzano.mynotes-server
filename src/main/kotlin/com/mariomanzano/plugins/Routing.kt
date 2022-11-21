ab1896004a26d2ac6ddeb7a2305366a60f5e6b5d
Mario Manzano <mario.manzano@logixsdigital.com>
2022-11-20 22:50:15 -0800

    Rest of CRUD Endpoints for Notes.

--- src/main/kotlin/com/mariomanzano/plugins/Routing.kt
+++ src/main/kotlin/com/mariomanzano/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.mariomanzano.plugins
 
+import com.mariomanzano.models.Note
 import com.mariomanzano.repositories.NotesRepository
 import io.ktor.server.routing.*
 import io.ktor.http.*
@@ -14,6 +15,18 @@ fun Application.configureRouting() {
         route("/notes"){
 
             // CREATE
+            post {
+                try {
+                    val note = call.receive<Note>()
+                    val savedNote = NotesRepository.save(note)
+                    call.respond(HttpStatusCode.Created, savedNote)
+                } catch (e: Exception) {
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        "Bad JSON Data Body: ${e.message.toString()}"
+                    )
+                }
+            }
 
             // READ
             get {
@@ -21,8 +34,37 @@ fun Application.configureRouting() {
             }
 
             // UPDATE
+            get("{id}") {
+                val id = call.parameters["id"] ?: return@get call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed id"
+                )
+
+                val note =
+                    NotesRepository.getById(id.toLong()) ?: return@get call.respond(
+                        HttpStatusCode.NotFound,
+                        "No note with id $id"
+                    )
+
+                call.respond(note)
+            }
 
             // DELETE
+            delete("{id}") {
+                val id = call.parameters["id"] ?: return@delete call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed id"
+                )
+
+                if (NotesRepository.delete(id.toLong())) {
+                    call.respond(HttpStatusCode.Accepted)
+                } else {
+                    call.respond(
+                        HttpStatusCode.NotFound,
+                        "No order with id $id"
+                    )
+                }
+            }
         }
     }
 }
